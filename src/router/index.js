ced6c5a9c573011181b7fe63ae0ec55efe236d0d
wyp <13263262903@163.com>
2017-12-29 18:55:09 +0800

    dev 提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import page1 from '../pages/page1.vue'
 
 Vue.use(Router)
 
@@ -12,10 +11,17 @@ export default new Router({
       name: 'HelloWorld',
       component: HelloWorld
     },
-    {
-      path: '/page1',
-      name: 'page1',
-      component: page1
+    {   // 当首次进入页面时，页面没有显示任何组件；让页面一加载进来就默认显示first页面
+      path: '/first', // 重定向，就是给它重新指定一个方向，加载一个组件
+      component: resolve => require(['@/pages/page1.vue'], resolve)
     }
+    // {
+    //   path: '/first',
+    //   component: resolve => require(['@/components/First'], resolve)
+    // },
+    // {
+    //   path: '/second',
+    //   component: resolve => require(['@/components/Second'], resolve)
+    // }
   ]
 })
