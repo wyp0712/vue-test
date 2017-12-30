4052c524ff7a9bfba93e7f0a7f51fa576fba5d6d
wyp <13263262903@163.com>
2017-12-30 22:56:30 +0800

    icon提交

---
+++ src/pages/page7.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <i class="el-icon-edit"></i>
+    <i class="el-icon-share"></i>
+    <i class="el-icon-delete"></i>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+  </div>
+</template>
+<script>
+  export default {
+    name: '',
+    data () {
+      return {
+        msg: 'this os masg'
+      }
+    },
+    created () {
+    }
+  }
+</script>
