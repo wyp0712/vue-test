5797738898cec5cdbb06ec5da0a24898d1e694b0
wyp <wangyipeng@360.net>
2018-01-23 10:12:07 +0800

    范德萨发

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 export default {
   name: 'app',
   created() {
+
   }
 }
 </script>
@@ -19,6 +20,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
